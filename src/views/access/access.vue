41f067d7fab41ce28a500ea51a6775b0a15ff2bb
chenhong <397148593@qq.com>
2018-04-17 18:27:47 +0800

    channel

--- src/views/access/access.vue
+++ src/views/access/access.vue
@@ -1,7 +1,6 @@
 <style lang="less">
     @import '../../styles/common.less';
     @import './access.less';
-
     @import '../../styles/table.less';
 </style>
 
@@ -16,7 +15,7 @@
         <Row type="flex" justify="end">
             <Col span="3" class="margin-bottom-10">
             <!--<router-link to="/index_new">-->
-                <!--<i-button @click="newUser">新增</i-button>-->
+            <!--<i-button @click="newUser">新增</i-button>-->
             <!--</router-link>-->
             <i-button @click="newUser">新增</i-button>
             </Col>
@@ -33,9 +32,11 @@
                         <i-input :value.sync="formValidate.name" placeholder="请输入渠道名称"></i-input>
                     </Form-item>
                     <Form-item label="认证方式" prop="submit">
-                        <Checkbox-group :model.sync="formValidate.submit">
-                            <Checkbox value="吃饭"></Checkbox>
-                        </Checkbox-group>
+                        <CheckboxGroup v-model.sync="formValidate.submit" v-for="item in checkways">
+                            <Checkbox label="item.name">
+                                <span>{{item.name}}</span>
+                            </Checkbox>
+                        </CheckboxGroup>
                     </Form-item>
                 </i-form>
             </div>
@@ -62,76 +63,75 @@
 </template>
 <!--id/name/link h5/submit认证方式/create_time/edit-->
 <script>
-    import {fetchList,submitList} from '@/api/channel'
+  import { fetchList, checkWays } from '@/api/channel'
 
-
-    export default {
-        data() {
-            return {
-                searchConName1: '',
-                newUserFlag: false,
-                editUserFlag:false,
-                columns1: [
-                    {
-                        title: 'ID',
-                        key: 'id'
-                    },
-                    {
-                        title: '渠道名称',
-                        key: 'name',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('strong', params.row.name)
-                            ]);
-                        }
-                    },
-                    {
-                        title: 'Link h5',
-                        key: 'link_h5'
-                    },
-                    {
-                        title: '认证方式',
-                        width: 150,
-                        key: 'check_ways_get',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('Tag', {
-                                    props: {
-                                        type: 'border',
-                                    },
-                                    style: {
-                                        marginRight: '2px'
-                                    },
-                                },params.row.check_ways_get),
-                                h('Tag', params.row.name)
-                            ]);
-                        }
-                    },
-                    {
-                        title: '创建时间',
-                        key: 'create_time'
-                    },
-                    {
-                        title: '操作',
-                        key: 'action',
-                        width: 150,
-                        align: 'center',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('Button', {
-                                    props: {
-                                        type: 'primary',
-                                        size: 'midddle'
-                                    },
-                                    style: {
-                                        marginRight: '5px'
-                                    },
-                                    on: {
-                                        click: () => {
-                                            this.show(params.row.id);
-                                        }
-                                    }
-                                }, '编辑'),
+  export default {
+    data () {
+      return {
+        searchConName1: '',
+        newUserFlag: false,
+        editUserFlag: false,
+        columns1: [
+          {
+            title: 'ID',
+            key: 'id'
+          },
+          {
+            title: '渠道名称',
+            key: 'name',
+            render: (h, params) => {
+              return h('div', [
+                h('strong', params.row.name)
+              ])
+            }
+          },
+          {
+            title: 'Link h5',
+            key: 'link_h5'
+          },
+          {
+            title: '认证方式',
+            width: 150,
+            key: 'check_ways_get',
+            render: (h, params) => {
+              return h('div', [
+                h('Tag', {
+                  props: {
+                    type: 'border',
+                  },
+                  style: {
+                    marginRight: '2px'
+                  },
+                }, params.row.name),
+                h('Tag', params.row.name)
+              ])
+            }
+          },
+          {
+            title: '创建时间',
+            key: 'create_time'
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 150,
+            align: 'center',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'midddle'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.row.id)
+                    }
+                  }
+                }, '编辑'),
 //                                h('Button', {
 //                                    props: {
 //                                        type: 'error',
@@ -143,94 +143,100 @@
 //                                        }
 //                                    }
 //                                }, 'Delete')
-                            ]);
-                        }
-                    }
-                ],
-                data1: [],
-                //modal-new
-                formValidate: {
-                    name: '',
-                    submit:''
-                },
-                ruleValidate: {
-                    name: [
-                        {required: true, message: '姓名不能为空', trigger: 'blur'}
-                    ],
-                    submit: [
-                        {required: true, type: 'array', min: 1, message: '至少选择一种认证方式', trigger: 'change'},
-                        {type: 'array', max: 2, message: '最多选择两种认证方式', trigger: 'change'}
-                    ],
-                }
+              ])
             }
+          }
+        ],
+        data1: [],
+        checkways:[],
+        //modal-new
+        formValidate: {
+          name: '',
+          submit: ''
         },
-        created() {
-            this.getChannelmodel()
-            this.getChannelSubmit()
+        formValidate: {
+          name: '',
+          submit: ''
         },
-        methods: {
-            getChannelSubmit(){
-                submitList().then(response => {
-                    this.formValidate.Submit = response.data.results;
-                    console.log("this.submit", this.response)
-                })
-            },
-            getChannelmodel() {
-                fetchList().then(response => {
-                    this.data1 = response.data.results;
-                })
-            },
-            newUser() {
-                //新增页面
-                this.newUserFlag=true;
-            },
-            search(data, argumentObj) {
-                // console.log("00", data, argumentObj)
-                let res = data;
-                let dataClone = data;
-                for (let argu in argumentObj) {
-                    console.log("argu", argu)
-                    if (argumentObj[argu].length > 0) {
-                        res = dataClone.filter(d => {
-                            return d[argu].indexOf(argumentObj[argu]) > -1;
-                        });
-                        dataClone = res;
-                    }
-                }
-                return res;
-            },
-            handleSearch1() {
-                this.data1 = this.search(this.data1, {name: this.searchConName1});
-            },
-            show(id) {
-                this.editUserFlag=true;
-                console.log("修改的当前客户的ID", id);
-            },
-            remove(index) {
-                // this.data6.splice(index, 1);
-            },
-            //模态框
-            confirmUser() {
-                this.newUserFlag=false;
-            },
-            cancelUser() {
-                this.newUserFlag=false;
-            },
-            //modal -edit
-            confirmEdit(){
-                this.editUserFlag=false;
-            },
-            cancelEdit(){
-                this.editUserFlag=false;
-            }
-
+        ruleValidate: {
+          name: [
+            {required: true, message: '姓名不能为空', trigger: 'blur'}
+          ],
+          submit: [
+            {required: true, type: 'array', min: 1, message: '至少选择一种认证方式', trigger: 'change'},
+            {type: 'array', max: 2, message: '最多选择两种认证方式', trigger: 'change'}
+          ],
         }
+      }
+    },
+    created () {
+      this.getChannelmodel()
+      this.getChannelSubmit()
+    },
+    methods: {
+      getChannelSubmit () {
+        checkWays().then(response => {
+          this.checkways = response.data.results;
+          console.log("认证方式",response.data.results)
+        })
+      },
+      getChannelmodel () {
+        fetchList().then(response => {
+          this.data1 = response.data.results
+          console.log('this.data1', this.data1)
+        })
+      },
+      newUser () {
+        //新增页面
+        this.newUserFlag = true
+      },
+      search (data, argumentObj) {
+        // console.log("00", data, argumentObj)
+        let res = data
+        let dataClone = data
+        for (let argu in argumentObj) {
+          console.log('argu', argu)
+          if (argumentObj[argu].length > 0) {
+            res = dataClone.filter(d => {
+              return d[argu].indexOf(argumentObj[argu]) > -1
+            })
+            dataClone = res
+          }
+        }
+        return res
+      },
+      handleSearch1 () {
+        this.data1 = this.search(this.data1, {name: this.searchConName1})
+      },
+      show (id) {
+        this.editUserFlag = true
+        console.log('修改的当前客户的ID', id)
+      },
+      remove (index) {
+        // this.data6.splice(index, 1);
+      },
+      //模态框
+      confirmUser () {
+        this.newUserFlag = false
+      },
+      cancelUser () {
+        this.newUserFlag = false
+      },
+      //modal -edit
+      confirmEdit () {
+        this.editUserFlag = false
+      },
+      cancelEdit () {
+        this.editUserFlag = false
+      }
+
     }
+  }
 </script>
 
 
 <style>
-    .ivu-modal{
-        top:30%;
+    .ivu-modal {
+        top: 30%;
     }
 </style>
